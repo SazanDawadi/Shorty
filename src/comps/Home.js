f45c99aaa6d47936491e7e5a3b14204736a5d1b1
SazanDawadi <sajandawadi5@gmail.com>
2021-05-22 08:20:20 +0545

    Url Validator added

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -7,21 +7,38 @@ import shrtCode from "../api/shrtcode";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import Banner from "../img/Banner.svg";
 
+ // eslint-disable-next-line
+const HTTP_URL_VALIDATOR_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+
 
 function Home() {
     const [url, setUrl] =  useState('');
     const [shrtUrl,setShrtUrl] = useState('');
     const[isLoading, setIsLoading] = useState(false);
     const[isCopied, setIsCopied] = useState(false);
+    const[isValidUrl, setIsValidUrl] = useState(true);
+
+      // Link Validation Function
+    const validateUrl = (string) => {
+    // Regex to check if string is a valid URL
+        return string.match(HTTP_URL_VALIDATOR_REGEX);
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        getUrl();
-        setUrl('');
-        setShrtUrl('')
-        setIsCopied(false);
+        if (validateUrl(url)){
+            getUrl();
+            setUrl('');
+            setShrtUrl('')
+            setIsCopied(false);
+            setIsLoading(!isLoading);
+            setIsValidUrl(true);
 
-        setIsLoading(!isLoading);
+        }
+        else{
+            setIsValidUrl(false);
+        }
+
     }
 
     const getUrl = async () => {
@@ -44,15 +61,21 @@ function Home() {
             <h1>Shorten Any Links</h1>
             <p>Build and protect your brand using powerful recognizable short link.</p>
             <form onSubmit={(e) => handleSubmit(e)} autoComplete='off' >
-                <TextField id = "home_input" label = "Type or Paste your link" variant = "outlined" value = {url}
+                <TextField id = "home_input" label = "Type or Paste your link" variant = "outlined" value = {url} 
                 onChange = {(e) =>{
                     setUrl(e.target.value)
                 }}
                 />
 
+                {
+                    !isValidUrl && (
+                        <div className = "display_error"><p>Invalid URL!</p></div>
+                    )
+                }
+
 
                 {!isLoading && (
-                    < Button id = "home_btn" variant = "contained" color = "lime" 
+                    < Button id = "home_btn" variant = "contained" color = "success " 
                     onClick = {(e) => handleSubmit(e)}>Shorten</Button>
                 )}
 
