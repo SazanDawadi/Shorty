1777371de4f347c05341fd80a4881545a67e1e7b
SazanDawadi <sajandawadi5@gmail.com>
2021-06-03 16:48:34 +0545

    Input form color updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import './App.css';
 import Home from "./comps/Home";
 import Nav from "./comps/Nav";
 import { BrowserRouter as Router } from 'react-router-dom';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
+import green from '@material-ui/core/colors/green'
 import Footer from './comps/Footer';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green
+  },
+});
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
-        <Home/>
-        <Footer />
-    </div>
+      <ThemeProvider theme = {theme} >
+        <div className="App">
+          <Nav />
+          <Home/>
+          <Footer />
+        </div>
+
+      </ThemeProvider>
+      
     </Router>
   );
 }
